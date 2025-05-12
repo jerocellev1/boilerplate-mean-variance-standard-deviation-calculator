b375766d7ec541ed5215bd1ddbef8450b5bf7317
Jerocelle <jerocellegingo@gmail.com>
2025-05-12 16:55:10 -0400

    Updated mean_var_std.py
    Updated with the right syntax to turn results into Python lists.

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,19 +1,21 @@
 import numpy as np
-import pandas as pd
-import random
+def calculate():
 
-def calculate(list):
+  first_list = [9,  1,  7,  8,  4,  6,  9, 10, 5]
+  np_list = np.array(first_list).reshape(3,3) # Convert list to a 3x3 numPy array named np_list
+  my_dict = {   
+    'mean': [np.mean(np_list, axis=0).tolist(), np.mean(np_list, axis=1).tolist(), np.mean(np_list.flatten()).tolist()],
+    'variance': [np.var(np_list, axis=0).tolist(), np.var(np_list, axis=1).tolist(), np.var(np_list.flatten()).tolist()],
+    'standard': [np.std(np_list, axis=0).tolist(), np.std(np_list, axis=1).tolist(), np.std(np_list.flatten()).tolist()],
+    'max': [np.max(np_list, axis=0).tolist(), np.max(np_list, axis=1).tolist(), np.max(np_list.flatten()).tolist()],
+    'min': [np.min(np_list, axis=0).tolist(), np.min(np_list, axis=1).tolist(), np.min(np_list.flatten()).tolist()],
+    'sum': [np.sum(np_list, axis=0).tolist(), np.sum(np_list, axis=1).tolist(), np.sum(np_list.flatten()).tolist()]
+    }
+  size = len(first_list) # Use the numpy array for size calculation
 
-list = np.array([random.randint(1,10)for _ in range(9)])
-list = {
-    'mean': [np.mean(list[0]).item(), np.mean(list[1]).item(), np.mean(list.flatten()).item()],
-    'variance': [np.var(list[0]).item(), np.var(list[1]).item(), np.var(list.flatten()).item()],
-    'standard deviation': [np.std(list[0]).item(), np.std(list[1]).item(), np.std(list.flatten()).item()],
-    'max': [np.max(list[0]).item(), np.max(list[1]).item(), np.max(list.flatten()).item()],
-    'min': [np.min(list[0]).item(), np.min(list[1]).item(), np.min(list.flatten()).item()],
-    'sum': [np.sum(list[0]).item(), np.sum(list[1]).item(), np.sum(list.flatten()).item()]
-}
+  if size != 9:
+    raise ValueError('List must contain nine numbers')
+  else:
+    return my_dict
 
-print(list)
-
-    return calculations
+calculate()
